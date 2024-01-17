8e2b2a6b1fe5e3525b4a54287e9f55eeb0e8a1f5
alebedev21 <a.lebedev@mvsystem.ru>
2024-01-17 15:09:22 +0300

    fix

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -146,7 +146,7 @@ app.MapPut("/user", [Authorize] async (HttpContext context, UserUpdateRequest re
     }
 });
 
-app.MapGet("/user", async (HttpContext context, IUserService service) =>
+app.MapGet("/user", [Authorize] async (HttpContext context, IUserService service) =>
 {
     var id = context.User.Claims.First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value!;
     Guid userId = new Guid(id);
